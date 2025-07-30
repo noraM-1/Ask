d1518254de41e89a5f64b05274df3c3e810a2428
Nora Martin <norakathleenmartin@gmail.com>
2025-07-30 14:39:37 -0400

    Initial Commit

---
+++ Ask/AskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskApp.swift
+//  Ask
+//
+//  Created by Amanda Martin on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
